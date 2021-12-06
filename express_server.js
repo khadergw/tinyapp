b3b6f365ba0c309f3ad8ff892da6d3cb60d083ef
GHadeer Khader <qods_a@yahoo.com>
2021-12-06 13:36:50 +0000

    changes to longURL

--- express_server.js
+++ express_server.js
@@ -5,16 +5,44 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 
-
-
-function generateRandomString() {
-
-}
-
+// let urlDatabase = {
+//   "b2xVn2": {
+//     shortURL: "b2xVn2",
+//     longURL: "http://www.lighthouselabs.ca",
+//     userID: "user1"
+//   },
+//   "9sm5xK": {
+//     shortURL: "9sm5xK",
+//     longURL: "http://www.google.com",
+//     userID: "user2"
+//   }
+// };
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+const users = { 
+  "user1": {
+    id: "user1", 
+    email: "user1@user.com", 
+    // password: "user1"
+    password: "$2a$10$Rb8Unenn5y7JCUPUQen8/.hwf1xmVKkrGhd0PP9KvyWVi8C9z5TJy"
+  },
+  "user2": {
+    id: "user2", 
+    email: "user2@user.com", 
+    password: "user2"
+  }
+};
+
+function generateRandomString() {
+  let output = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) output += possible.charAt(Math.floor(Math.random() * possible.length));
+  return output;
+}
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -47,12 +75,18 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shorturl: req.params.shortURL, longURL:urlDatabase};
+  const templateVars = { shorturl: req.params.shortURL, longURL:urlDatabase[req.params.shortURL].longURL};
   res.render("urls_show", templateVars);
 });
 
 
-
+app.get('/u/:shortURL', (req, res) => {
+  if (urlDatabase.hasOwnProperty(req.params.shortURL)) {
+    const longURL = urlDatabase[req.params.shortURL].longURL;
+    res.redirect(longURL);
+  }
+  res.redirect('/urls');
+});
 
 
 app.listen(PORT, () => {
@@ -60,4 +94,4 @@ app.listen(PORT, () => {
 });
 
 
-generateRandomString()
+//generateRandomString()
